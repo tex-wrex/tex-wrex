614823107c2d6913692456473526378210a7da20
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-06-04 18:03:15 -0500

    added functions file py file and more viuals

--- Fermin/functions.py
+++ Fermin/functions.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 from glob import glob
 
 
@@ -52,3 +54,18 @@ def process_csv_files(csv_files):
     df_svc.to_csv('master_crashes_svc.csv', index=False)
 
     return df_svc
+
+def split(df):
+    '''
+    This function splits a dataframe into 
+    train, validate, and test in order to explore the data and to create and validate models. 
+    It takes in a dataframe and contains an integer for setting a seed for replication. 
+    Test is 20% of the original dataset. The remaining 80% of the dataset is 
+    divided between valiidate and train, with validate being .30*.80= 24% of 
+    the original dataset, and train being .70*.80= 56% of the original dataset. 
+    The function returns, train, validate and test dataframes. 
+    '''
+    train, test = train_test_split(df, test_size = .2, random_state=123)   
+    train, validate = train_test_split(train, test_size=.3, random_state=123)
+    
+    return train, validate, test
