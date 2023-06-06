234745b3df5fb63ef5e36d6bb60d6f90f6aac555
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-06-06 11:48:49 -0500

    explore.py

--- Final_work/explore.py
+++ Final_work/explore.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from scipy.stats import chi2_contingency
-
+import matplotlib.pyplot as plt
 
 def perform_chi_square_test(df, column_name, injury_column):
     # Create the contingency table
@@ -13,3 +13,15 @@ def perform_chi_square_test(df, column_name, injury_column):
     print("Chi-square test statistic:", chi2)
     print("P-value:", p_value)
     print("Degrees of freedom:", dof)
+    
+    
+
+
+def plot_contingency_table(df, column1, column2):
+    # Create the contingency table
+    contingency_table = pd.crosstab(df[column1], df[column2])
+
+    # Plot the contingency table as a bar chart
+    contingency_table.plot(kind='bar', figsize=(20, 20))
+    plt.show()
+
