108be1f708ad079d0cfe628eb1608b04d12aefb5
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-06-06 11:42:52 -0500

    updated explore

---
+++ Final_work/explore.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+
+def perform_chi_square_test(df, gender_column, injury_column):
+    # Create the contingency table
+    contingency_table = pd.crosstab(df[gender_column], df[injury_column])
+    
+    # Perform the chi-square test
+    chi2, p_value, dof, expected = chi2_contingency(contingency_table)
+    
+    # Print the results
+    print("Chi-square test statistic:", chi2)
+    print("P-value:", p_value)
+    print("Degrees of freedom:", dof)
