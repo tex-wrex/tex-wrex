22ba158adeee8d310470c1ce87a620111ce6f25e
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-06-06 14:14:01 -0500

    wrangle file added

--- Final_work/wrangle.py
+++ Final_work/wrangle.py
@@ -1,7 +1,5 @@
 import os
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
 
 def acquire_motocycle_data():
     if os.path.exists('motocycle_data.csv'):
@@ -69,23 +67,62 @@ def prepare_filtered_dataset_version():
         new_df.to_csv('master_modeling.csv', index=False)
         return new_df
     
-    
 # ==========================================================================================
 # ==========================================================================================
 # ==========================================================================================
-
-
-def split(df):
+  
+def prepare_second_filtered_dataset_version():
     '''
-    This function splits a dataframe into 
-    train, validate, and test in order to explore the data and to create and validate models. 
-    It takes in a dataframe and contains an integer for setting a seed for replication. 
-    Test is 20% of the original dataset. The remaining 80% of the dataset is 
-    divided between valiidate and train, with validate being .30*.80= 24% of 
-    the original dataset, and train being .70*.80= 56% of the original dataset. 
-    The function returns, train, validate and test dataframes. 
-    '''
-    train, test = train_test_split(df, test_size = .2, random_state=123)   
-    train, validate = train_test_split(train, test_size=.3, random_state=123)
+    Takes the dataset from the 'acquire_motocycle_data()' function and filters out
+    unnecessary data in order to replicate the dataset from Gabe's
+    'master_modeling.csv' file.
     
-    return train, validate, test
+    INPUT:
+    NONE
+    
+    OUTPUT:
+    master_modeling_updated.csv = IF NONEXISTANT, .csv file of the filtered dataset
+    new_df = Pandas dataframe of the filtered dataset
+    '''
+    if os.path.exists('master_modeling_updated.csv'):
+        new_df = pd.read_csv('master_modeling_updated.csv', index_col=0)
+        return new_df
+    else:
+        df = prepare_filtered_dataset_version()
+        df.drop('vehicle_defect_1', axis=1, inplace=True)
+        vehicle_make_most_frequent = df['vehicle_make'].value_counts().idxmax()
+        df['vehicle_make'] = df['vehicle_make'].replace('other', vehicle_make_most_frequent)
+        df['vehicle_model_name'] = df['vehicle_model_name'].str.replace(r'\(.*\)', '', regex=True)
+        df['vehicle_model_name'] = df['vehicle_model_name'].str.strip()
+        unknown_model_indices = df[df['vehicle_model_name'] == 'unknown'].index
+        for idx in unknown_model_indices:
+            vehicle_make = df.loc[idx, 'vehicle_make']
+            most_frequent_model = df[df['vehicle_make'] == vehicle_make]['vehicle_model_name'].value_counts().idxmax()
+            df.loc[idx, 'vehicle_model_name'] = most_frequent_model
+        df.rename(columns={'vehicle_model_name': 'vehicle_model'}, inplace=True)
+        other_count = df[df['vehicle_model'].str.contains('other')].shape[0]
+        other_model_indices = df[df['vehicle_model'].str.contains('other')].index
+        for idx in other_model_indices:
+            vehicle_make = df.loc[idx, 'vehicle_make']
+            most_frequent_model = df[df['vehicle_make'] == vehicle_make]['vehicle_model'].value_counts().idxmax()
+            df.loc[idx, 'vehicle_model'] = most_frequent_model
+        df['vehicle_model_year'] = df['vehicle_model_year'].astype(str)
+        df['vehicle_model_year'] = df['vehicle_model_year'].str.rstrip('.0')
+        df.to_csv('master_modeling_updated.csv', index=False)
+        df = pd.read_csv('master_modeling_updated.csv')
+        vehicle_make_most_frequent = df['vehicle_make'].value_counts().idxmax()
+        df['vehicle_make'] = df['vehicle_make'].replace(['other (explain in narrative)', 'unknown'], vehicle_make_most_frequent)
+        changes_made = df['vehicle_make'].value_counts()[vehicle_make_most_frequent] - len(df[df['vehicle_make'] == vehicle_make_most_frequent])
+        other_unknown_model_indices = df[df['vehicle_model'].str.contains('other|unknown')].index
+        for idx in other_unknown_model_indices:
+            vehicle_make = df.loc[idx, 'vehicle_make']
+            most_frequent_model = df[df['vehicle_make'] == vehicle_make]['vehicle_model'].value_counts().idxmax()
+            second_most_frequent_model = df[df['vehicle_make'] == vehicle_make]['vehicle_model'].value_counts().index[1] if len(df[df['vehicle_make'] == vehicle_make]['vehicle_model'].value_counts()) > 1 else most_frequent_model
+            if 'other' in most_frequent_model or 'unknown' in most_frequent_model:
+                df.loc[idx, 'vehicle_model'] = second_most_frequent_model
+            else:
+                df.loc[idx, 'vehicle_model'] = most_frequent_model
+        changes_made = len(other_unknown_model_indices)
+        new_df = df
+        new_df.to_csv('master_modeling_updated.csv', index=False)
+        return df
